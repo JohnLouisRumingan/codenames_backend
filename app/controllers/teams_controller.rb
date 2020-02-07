35b43ff6d11d492d5530199d45b62a3a882b3bf0
John Louis Rumingan <jmrumingan@gmail.com>
2020-02-07 12:45:58 -0500

    Added basic api json

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,12 @@
 class TeamsController < ApplicationController
+
+    def index 
+        teams = Team.all 
+        render json: teams 
+    end 
+
+    def show 
+        team = Team.find_by(id: params[:id])
+        render json: team 
+    end 
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,12 @@
 class WordsController < ApplicationController
+
+    def index 
+        words = Word.all 
+        render json: words 
+    end 
+
+    def show 
+        word = Word.find_by(id: params[:id])
+        render json: word
+    end 
 end
